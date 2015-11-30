3e0b8ded81b328b68034db6c101c147b6aa1e349
Martin <112505290@qq.com>
2015-11-30 17:36:49 +0800

    123

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -21,7 +21,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"123");
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
